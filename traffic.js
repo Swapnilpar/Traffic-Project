81f3231e02879d22d24bc5e221020bc246e05461
Swapnil Pareek <swpnlprk@gmail.com>
2024-05-13 11:28:40 +0530

    first commit of traffic project

---
+++ traffic.js
@@ -0,0 +1,432 @@
+var apiKey = "eoyoO5587Mm44dopGhB9qwQJMJoFGZJi";
+var centerCoords = [77.2197, 28.6315];
+var initialZoom = 16;
+var map = tt.map({
+    key: apiKey,
+    container: "map",
+    center: centerCoords,
+    zoom: initialZoom,
+    language: "en-US"
+});
+
+var searchBoxInstance;
+var startCornerLngLat;
+var endCornerLngLat;
+var mousePressed;
+var drawBoundingBoxButtonPressed;
+var layerFillID = "layerFillID";
+var layerOutlineID = "layerOutlineID";
+var sourceID = "sourceID";
+var styleBase = "tomtom://raster/1/";
+var styleS1 = "s2";
+var styleRelative = "relative";
+var refreshTimeInMillis = 30000;
+var popupHideDelayInMilis = 4000;
+var incidentListContainer = document.getElementById("incident-list");
+var trafficFlowTilesToggle = document.getElementById("flow-toggle");
+
+var trafficIncidentsTier = new tt.TrafficIncidentTier({
+    key: apiKey,
+    incidentDetails: {
+        style: styleS1
+    },
+    incidentTiles: {
+        style: styleBase + styleS1,
+    },
+    refresh: refreshTimeInMillis
+});
+
+var trafficFlowTilesTier = new tt.TrafficFlowTilesTier({
+    key: apiKey,
+    style: styleBase + styleRelative,
+    refresh: refreshTimeInMillis
+});
+
+var commonSearchBoxOptions = {
+    key: apiKey,
+    center: map.getCenter()
+};
+
+
+
+
+
+function toggleTrafficFlowTilesTier() {
+    if (trafficFlowTilesToggle.checked) {
+        map.addTier(trafficFlowTilesTier);
+    } else {
+        map.removeTier(trafficFlowTilesTier.getId());
+    }
+}
+
+function showTrafficIncidentsTier() {
+    document.getElementById("incidents-toggle").checked = true;
+    map.addTier(trafficIncidentsTier);
+}
+
+function hideTrafficIncidentsTier() {
+    document.getElementById("incidents-toggle").checked = false;
+    map.removeTier(trafficIncidentsTier.getId());
+    clearIncidentList();
+    removeBoundingBox();
+}
+
+function toggleTrafficIncidentsTier() {
+    if (document.getElementById("incidents-toggle").checked) {
+        showTrafficIncidentsTier();
+    } else {
+        hideTrafficIncidentsTier();
+    }
+}
+
+function updateSearchBoxOptions() {
+    var updatedOptions = Object.assign(commonSearchBoxOptions, {
+        center: map.getCenter()
+    });
+    searchBoxInstance.updateOptions({
+        minNumberOfCharacters: 0,
+        searchOptions: updatedOptions,
+        autocompleteOptions: updatedOptions
+    });
+}
+
+function onSearchBoxResult(result) {
+    map.flyTo({
+        center: result.data.result.position,
+        speed: 3
+    });
+}
+
+function enableBoundingBoxDraw() {
+    showInfoPopup("Click and drag to draw a box on desired area");
+    drawBoundingBoxButtonPressed = true;
+    removeBoundingBox();
+    clearIncidentList();
+}
+
+function getPopupWrapper() {
+    return document.getElementById("popup-wrapper");
+}
+
+function showPopup(element) {
+    element.style.opacity = "0.9";
+}
+
+function showInfoPopup(message) {
+    var popupElementDiv = getPopupWrapper();
+    popupElementDiv.innerHTML = getPopupInnerHTML("popup-info", message);
+    showPopup(popupElementDiv);
+}
+
+function showErrorPopup(message) {
+    var popupElementDiv = getPopupWrapper();
+    popupElementDiv.innerHTML = getPopupInnerHTML("popup-error", message);
+    showPopup(popupElementDiv);
+}
+
+function hidePopup(delayInMilis) {
+    var element = getPopupWrapper();
+    if (delayInMilis == 0) {
+        element.style.opacity = "0";
+    } else {
+        setTimeout(function () {
+            element.style.opacity = "0";
+        }, delayInMilis);
+    }
+}
+
+function getPopupInnerHTML(popupClass, popupMessage) {
+    return `<div class="container ${popupClass} popup"> <div class="row"> <div class="col py-2"> <div class="row align-items-center pt-1"> <div class="col-sm-1"> <img src="img/error-symbol.png" alt=""/> </div><div id="popup-message" class="col"> ${popupMessage} </div></div></div></div></div>`;
+}
+
+function removeBoundingBox() {
+    if (map.getSource(sourceID)) {
+        map.removeLayer(layerFillID);
+        map.removeLayer(layerOutlineID)
+        map.removeSource(sourceID);
+    }
+}
+
+function onMouseDown(eventDetails) {
+    if (drawBoundingBoxButtonPressed) {
+        eventDetails.preventDefault();
+        mousePressed = true;
+        startCornerLngLat = eventDetails.lngLat;
+        removeBoundingBox();
+        map.addSource(sourceID, getPolygonSource(startCornerLngLat, startCornerLngLat));
+        map.addLayer({
+            id: layerFillID,
+            type: "fill",
+            source: sourceID,
+            layout: {},
+            paint: {
+                "fill-color": "#666",
+                "fill-opacity": 0.1
+            }
+        });
+        map.addLayer({
+            id: layerOutlineID,
+            type: "line",
+            source: sourceID,
+            layout: {},
+            paint: {
+                "line-width": 4,
+                "line-color": "#424242",
+                "line-dasharray": [2, 1],
+                "line-blur": 0.5
+            }
+        });
+    }
+}
+
+function onMouseMove(eventDetails) {
+    if (mousePressed) {
+        endCornerLngLat = eventDetails.lngLat;
+        updateRectangleData(startCornerLngLat, endCornerLngLat);
+    }
+}
+
+function onMouseUp(eventDetails) {
+    mousePressed = false;
+    hidePopup(0);
+    if (drawBoundingBoxButtonPressed) {
+        endCornerLngLat = eventDetails.lngLat;
+        if (bothLngLatAreDifferent(startCornerLngLat, endCornerLngLat)) {
+            updateRectangleData(startCornerLngLat, endCornerLngLat);
+            clearIncidentList();
+            displayTrafficIncidents(getLngLatBoundsForIncidentDetailsCall(startCornerLngLat, endCornerLngLat));
+            showTrafficIncidentsTier();
+        } else {
+            showErrorPopup("Try to select bigger box.");
+            hidePopup(popupHideDelayInMilis);
+        }
+    }
+    drawBoundingBoxButtonPressed = false;
+}
+
+function bothLngLatAreDifferent(lngLat1, lngLat2) {
+    return lngLat1.lat !== lngLat2.lat && lngLat1.lng !== lngLat2.lng;
+}
+
+function updateRectangleData(startCornerLngLat, endCornerLngLat) {
+    map.getSource(sourceID).setData(getPolygonSourceData(startCornerLngLat, endCornerLngLat));
+}
+
+function getLngLatBoundsForIncidentDetailsCall(startCornerLngLat, endCornerLngLat) {
+    var bottomLeftCorner = new tt.LngLat(
+        startCornerLngLat.lng < endCornerLngLat.lng ? startCornerLngLat.lng : endCornerLngLat.lng,
+        startCornerLngLat.lat < endCornerLngLat.lat ? startCornerLngLat.lat : endCornerLngLat.lat);
+    var topRightCorner = new tt.LngLat(
+        startCornerLngLat.lng > endCornerLngLat.lng ? startCornerLngLat.lng : endCornerLngLat.lng,
+        startCornerLngLat.lat > endCornerLngLat.lat ? startCornerLngLat.lat : endCornerLngLat.lat);
+    return tt.LngLatBounds.convert([bottomLeftCorner.toArray(), topRightCorner.toArray()]);
+}
+
+function getPolygonSourceData(startCornerLngLat, endCornerLngLat) {
+    return {
+        type: "Feature",
+        geometry: {
+            type: "Polygon",
+            coordinates: [
+                [
+                    [startCornerLngLat.lng, startCornerLngLat.lat],
+                    [startCornerLngLat.lng, endCornerLngLat.lat],
+                    [endCornerLngLat.lng, endCornerLngLat.lat],
+                    [endCornerLngLat.lng, startCornerLngLat.lat],
+                    [startCornerLngLat.lng, startCornerLngLat.lat]
+                ]
+            ]
+        }
+    };
+}
+
+function getPolygonSource(startCornerLngLat, endCornerLngLat) {
+    return {
+        type: "geojson",
+        data: getPolygonSourceData(startCornerLngLat, endCornerLngLat)
+    };
+}
+
+function clearIncidentList() {
+    incidentListContainer.innerHTML = "";
+}
+
+function isCluster(incident) {
+    return incident.id.includes("CLUSTER");
+}
+
+
+// var suggestRoutesButton = document.getElementById("suggest-routes-button");
+
+// function toggleSuggestRoutesButton(enable) {
+//   suggestRoutesButton.disabled = !enable;
+// }
+
+
+// function displayTrafficIncidents(boundingBox) {
+//     var iconsMapping = ["danger", "accident", "fog", "danger", "rain", "ice", "incident", "laneclosed", "roadclosed", "roadworks", "wind", "flooding", "detour", ""];
+//     var delayMagnitudeMapping = ["unknown", "minor", "moderate", "major", "undefined"];
+
+//     tt.services.incidentDetails({
+//             key: apiKey,
+//             boundingBox: boundingBox,
+//             style: styleS1,
+//             zoomLevel: parseInt(map.getZoom())
+//         })
+//         .go()
+//         .then(function (results) {
+//             if (results.tm.poi.length === 0) {
+//                 showErrorPopup("There are no traffic activities in this area 🥳🥳");
+//                 hidePopup(popupHideDelayInMilis);
+//             } else {
+//                 results.tm.poi.forEach(function (incident) {
+//                     var buttonListItem = createButtonItem(incident.p);
+
+//                     if (isCluster(incident)) {
+//                         buttonListItem.innerHTML = getButtonClusterContent(incident.id, incident.cs, delayMagnitudeMapping[incident.ty]);
+//                         incidentListContainer.appendChild(buttonListItem);
+//                     } else {
+//                         buttonListItem.innerHTML = getButtonIncidentContent(incident.d.toUpperCase(), iconsMapping[incident.ic], delayMagnitudeMapping[incident.ty], incident.f, incident.t);
+//                         incidentListContainer.appendChild(buttonListItem);
+//                     }
+//                 });
+
+//                 if (results.tm.poi.length > 0) {
+//                     toggleSuggestRoutesButton(true);
+//                   } else {
+//                     toggleSuggestRoutesButton(false);
+//                   }
+//             }
+//         });
+// }
+
+// suggestRoutesButton.addEventListener("click", function () {
+//     // Check if there are traffic incidents
+//     var boundingBox =  {
+//         west: -74.2591, // Longitude of the southwest corner
+//         south: 40.4774, // Latitude of the southwest corner
+//         east: -73.7002, // Longitude of the northeast corner
+//         north: 40.9176  // Latitude of the northeast corner
+//       };
+//     tt.services.incidentDetails({
+//       key: apiKey,
+//       boundingBox: boundingBox,
+//       style: styleS1,
+//       zoomLevel: parseInt(map.getZoom())
+//     })
+//       .go()
+//       .then(function (results) {
+//         if (results.tm.poi.length > 0) {
+//           // There are traffic incidents, suggest alternative routes
+//           tt.services.routing({
+//             key: apiKey,
+//             locations:  [
+//                 { lat: 40.748817, lon: -73.985428 }, // Start point
+//                 { lat: 40.748817, lon: -74.005428 }  // End point
+//               ],
+//             traffic: true,
+//             alternatives: true
+//           })
+//             .go()
+//             .then(function (response) {
+//               if (response && response.routes) {
+//                 // Update your UI with the suggested alternative routes information
+//                 console.log("Suggested Alternative Routes:", response.routes);
+//                 // You can implement further UI updates or actions based on the suggested routes
+//               } else {
+//                 console.error("No alternative routes found.");
+//               }
+//             })
+//             .catch(function (error) {
+//               console.error("Error fetching alternative routes:", error);
+//             });
+//         } else {
+//           // No traffic incidents, provide a message to the user
+//           console.log("No traffic incidents found. Alternative routes not suggested.");
+//         }
+//       });
+//   });
+
+
+function displayTrafficIncidents(boundingBox) {
+    var iconsMapping = ["danger", "accident", "fog", "danger", "rain", "ice", "incident", "laneclosed", "roadclosed", "roadworks", "wind", "flooding", "detour", ""];
+    var delayMagnitudeMapping = ["unknown", "minor", "moderate", "major", "undefined"];
+
+    tt.services.incidentDetails({
+            key: apiKey,
+            boundingBox: boundingBox,
+            style: styleS1,
+            zoomLevel: parseInt(map.getZoom())
+        })
+        .go()
+        .then(function (results) {
+            if (results.tm.poi.length === 0) {
+                showErrorPopup("There are no traffic incidents in this area.");
+                hidePopup(popupHideDelayInMilis);
+            } else {
+                results.tm.poi.forEach(function (incident) {
+                    var buttonListItem = createButtonItem(incident.p);
+
+                    if (isCluster(incident)) {
+                        buttonListItem.innerHTML = getButtonClusterContent(incident.id, incident.cs, delayMagnitudeMapping[incident.ty]);
+                        incidentListContainer.appendChild(buttonListItem);
+                    } else {
+                        buttonListItem.innerHTML = getButtonIncidentContent(incident.d.toUpperCase(), iconsMapping[incident.ic], delayMagnitudeMapping[incident.ty], incident.f, incident.t);
+                        incidentListContainer.appendChild(buttonListItem);
+                    }
+                });
+            }
+        });
+}
+
+function createButtonItem(incidentPosition) {
+    var incidentBtn = document.createElement("button");
+    incidentBtn.setAttribute("type", "button");
+    incidentBtn.classList.add("list-group-item", "list-group-item-action", "incidendDetailsListItemButton");
+    incidentBtn.addEventListener("click", function () {
+        map.flyTo({
+            center: incidentPosition
+        });
+    }, false);
+
+    return incidentBtn;
+}
+
+function getButtonIncidentContent(description, iconCategory, delayMagnitude, fromAddress, toAddress) {
+    return `<div class="row align-items-center pb-2"> <div class="col-sm-2"> <div class="tt-traffic-icon"> <div class="tt-icon-circle-${delayMagnitude} traffic-icon"> <div class="tt-icon-${iconCategory}"></div></div></div></div><div class="col label pl-0"> ${description} </div></div><div class="row"> <div class="col-sm-2"><label class="label">From: </label></div><div class="col"><label class="incident-details-list-normal-text">${fromAddress}</label> </div></div><div class="row"> <div class="col-sm-2"><label class="label">To: </label></div><div class="col"><label class="incident-details-list-normal-text">${toAddress}</label></div></div>`;
+}
+
+function getButtonClusterContent(description, numberOfIncidents, delayMagnitude) {
+    return `<div class="row align-items-center pb-2"> <div class="col-sm-2"> <div class="tt-traffic-icon"> <div class="tt-icon-circle-${delayMagnitude} traffic-icon"> <div id="cluster-icon" class="tt-icon-number">${numberOfIncidents}</div></div></div></div><div class="col label pl-0"> ${description} </div></div>`;
+}
+
+
+
+
+
+
+function initApplication() {
+    searchBoxInstance = new tt.plugins.SearchBox(tt.services, {
+        minNumberOfCharacters: 0,
+        labels: {
+            placeholder: "Search"
+        },
+        noResultsMessage: "No results found.",
+        searchOptions: commonSearchBoxOptions,
+        autocompleteOptions: commonSearchBoxOptions
+    });
+
+    searchBoxInstance.on("tomtom.searchbox.resultselected", onSearchBoxResult);
+
+    document.getElementById("search-panel").append(searchBoxInstance.getSearchBoxHTML());
+    trafficFlowTilesToggle.addEventListener("change", toggleTrafficFlowTilesTier);
+    document.getElementById("incidents-toggle").addEventListener("change", toggleTrafficIncidentsTier);
+    document.getElementById("bounding-box-button").addEventListener("click", enableBoundingBoxDraw);
+
+    map.on("mousedown", onMouseDown);
+    map.on("mouseup", onMouseUp);
+    map.on("mousemove", onMouseMove);
+    map.on("moveend", updateSearchBoxOptions);
+
+}
+initApplication();
